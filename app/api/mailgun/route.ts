47be334d9ba274330b16eb46e20f7c8698e870d6
Thomas Kady <thomas.kady@gmail.com>
2023-05-17 12:30:07 -0500

    comment out mailgun functionality

--- app/api/mailgun/route.ts
+++ app/api/mailgun/route.ts
@@ -4,16 +4,16 @@ const mailgun = new Mailgun(formData);
 const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
 
 export const GET = async () => {
-    console.log('test');
-    mg.messages.create('sandboxac2c34f7810c44a5af71939d429ec327.mailgun.org', {
-        from: "Round Table <mailgun@sandboxac2c34f7810c44a5af71939d429ec327.mailgun.org>",
-        to: ["19709806253@tmomail.net"],
-        subject: "Good morning",
-        text: "Start today's journey <link>",
-        html: "<h1>Start today's journey!</h1>"
-    })
-    .then(msg => console.log(msg))
-    .catch(err => console.log(err));
+    // console.log('test');
+    // mg.messages.create('sandboxac2c34f7810c44a5af71939d429ec327.mailgun.org', {
+    //     from: "Round Table <mailgun@sandboxac2c34f7810c44a5af71939d429ec327.mailgun.org>",
+    //     to: ["19709806253@tmomail.net"],
+    //     subject: "Good morning",
+    //     text: "Start today's journey <link>",
+    //     html: "<h1>Start today's journey!</h1>"
+    // })
+    // .then((msg:any) => console.log(msg))
+    // .catch((err: any) => console.log(err));
 
 
     const response = 'TEST';
