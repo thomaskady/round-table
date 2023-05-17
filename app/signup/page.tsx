163ddf3e938902276974b8cb74bd2416b3e36677
Thomas Kady <thomas.kady@gmail.com>
2023-05-17 03:31:47 -0500

    add form component

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,25 @@
+import styles from './page.module.css';
+
 export default function SignUp() {
-    return <div>Sign up</div>
+    return (
+        <div className={styles.container}>
+            <form className={styles.form}>
+                <div className={styles.inputs}>
+                    <div className={styles.input}>
+                        <label htmlFor='username'>Username:</label>
+                        <input  type='text' id='username' name='username' required></input>
+                    </div>
+                    <div className={styles.input}>
+                        <label htmlFor='password'>Password:</label>
+                        <input type='password' id='password' name='password' required></input>
+                    </div>
+                    <div className={styles.input}>
+                        <label htmlFor='number'>Phone Number:</label>
+                        <input type='tel' id='number' name='number' required placeholder='+1XXXXXXXXXX'></input>
+                    </div>
+                    <button className={styles.button}>Sign up</button>
+                </div>
+            </form>
+        </div>
+    )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './Nav.module.css';
 import Button from "./Button";
@@ -7,7 +8,9 @@ export default function Nav() {
 
     return (
         <nav className={styles.nav}>
-            <Image src='/round-table.png' alt='The Round Table Logo' width={100} height={100}></Image>
+            <Link href='/'>
+                <Image src='/round-table.png' alt='The Round Table Logo' width={100} height={100}></Image>
+            </Link>
             <Button buttonType='signup'></Button>
         </nav>
     )
