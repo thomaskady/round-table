28e422662f68d9f23b2e5e6febfce366b4482d67
Thomas Kady <thomas.kady@gmail.com>
2023-05-17 12:35:04 -0500

    add default key for mailgun

--- app/api/mailgun/route.ts
+++ app/api/mailgun/route.ts
@@ -1,7 +1,7 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'your key here'});
 
 export const GET = async () => {
     // console.log('test');
